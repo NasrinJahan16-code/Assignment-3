503aa42b7167256920a56bd4113492bb976ab976
NasrinJahan16-code <nasrinjh1696@gmail.com>
2024-09-06 10:14:05 -0700

    added an index.html file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('assets/bg.jpg')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+        }
+      },
+    
+    },
+    
